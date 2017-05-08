4bdddd086cf0fad9e40625b0ae98a6ebfc1f4781
Karoly Erdos <karoly@ebi.ac.uk>
2017-05-08 11:25:36 +0100

    Add the missing entry point for the Validation Aggregator application

---
+++ src/main/java/uk/ac/ebi/subs/validator/ValidatorAggregatorApplication.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.subs.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point of the Validator Aggregator application.
+ *
+ * Created by karoly on 08/05/2017.
+ */
+@SpringBootApplication
+public class ValidatorAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidatorAggregatorApplication.class, args);
+    }
+
+}
