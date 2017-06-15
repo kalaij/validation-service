1ad295cd9b1033ed347498b68054d0897ddc3e1c
Flavia Penim <flaviapenim@gmail.com>
2017-06-15 10:23:23 +0100

    Create messaging configuration.

---
+++ src/main/java/uk/ac/ebi/subs/validator/core/messaging/CoreQueueConfiguration.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.subs.validator.core.messaging;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import uk.ac.ebi.subs.validator.messaging.Queues;
+import uk.ac.ebi.subs.validator.messaging.RoutingKeys;
+import uk.ac.ebi.subs.validator.messaging.ValidationExchangeConfig;
+
+@Configuration
+@ComponentScan(basePackageClasses = ValidationExchangeConfig.class)
+public class CoreQueueConfiguration {
+
+    @Bean
+    public Queue coreValidationQueue() {
+        return new Queue(Queues.CORE_SAMPLE_VALIDATION, true);
+    }
+
+    @Bean
+    public Binding coreValidationBinding(Queue coreValidationQueue, TopicExchange validationExchange) {
+        return BindingBuilder.bind(coreValidationQueue).to(validationExchange).with(RoutingKeys.EVENT_CORE_VALIDATION);
+    }
+}
