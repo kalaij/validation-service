1e93f3ba426d8ae525c474555a61e3b75a147294
Flavia Penim <flaviapenim@gmail.com>
2017-06-16 18:11:04 +0100

    Add initial validators structure.

--- src/main/java/uk/ac/ebi/subs/validator/core/ValidatorListener.java
+++ src/main/java/uk/ac/ebi/subs/validator/core/ValidatorListener.java
@@ -7,8 +7,12 @@ import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.subs.validator.data.SingleValidationResult;
 import uk.ac.ebi.subs.validator.data.ValidationMessageEnvelope;
+import uk.ac.ebi.subs.validator.data.ValidationStatus;
+import uk.ac.ebi.subs.validator.messaging.Exchanges;
 import uk.ac.ebi.subs.validator.messaging.Queues;
+import uk.ac.ebi.subs.validator.messaging.RoutingKeys;
 
 @Service
 public class ValidatorListener {
@@ -27,5 +31,14 @@ public class ValidatorListener {
         logger.debug("Validation request received.");
 
         //TODO
+    }
+
+    private void sendResults(SingleValidationResult singleValidationResult) {
+        if (singleValidationResult.getValidationStatus().equals(ValidationStatus.Error)) {
+            rabbitMessagingTemplate.convertAndSend(Exchanges.VALIDATION, RoutingKeys.EVENT_VALIDATION_ERROR, singleValidationResult);
+        } else {
+            rabbitMessagingTemplate.convertAndSend(Exchanges.VALIDATION, RoutingKeys.EVENT_VALIDATION_SUCCESS, singleValidationResult);
+
+        }
     }
 }
---
+++ src/main/java/uk/ac/ebi/subs/validator/core/validators/SampleRelationshipValidator.java
@@ -0,0 +1,4 @@
+package uk.ac.ebi.subs.validator.core.validators;
+
+public class SampleRelationshipValidator {
+}
