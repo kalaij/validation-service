1088d3a132a8e5dc578861d5edf77e67744451e8
Karoly Erdos <karoly@ebi.ac.uk>
2017-07-03 13:36:06 +0100

    Refactor logic in service: upserting the ValidationResult document

--- src/main/java/uk/ac/ebi/subs/validator/coordinator/ValidationResultService.java
+++ src/main/java/uk/ac/ebi/subs/validator/coordinator/ValidationResultService.java
@@ -4,18 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.ac.ebi.subs.data.component.Archive;
 import uk.ac.ebi.subs.data.submittable.BaseSubmittable;
+import uk.ac.ebi.subs.validator.data.ValidationAuthor;
 import uk.ac.ebi.subs.validator.data.ValidationResult;
 import uk.ac.ebi.subs.validator.repository.ValidationResultRepository;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class ValidationResultService {
@@ -28,53 +25,31 @@ public class ValidationResultService {
         logger.debug("Creating Validation Result Document for {} from submission {}",
                 submittable.getClass().getSimpleName(), submissionId);
 
-        int version = getVersion(submissionId, submittable.getId());
-        ValidationResult validationResult = generateValidationResult(submissionId, submittable, version);
+        return createOrUpdateValidationResult(submissionId, submittable.getId());
+    }
+
+    ValidationResult createOrUpdateValidationResult(String submissionId, String submittableUuid) {
+        ValidationResult validationResult = repository.findByEntityUuid(submittableUuid);
+        if (validationResult != null) {
+            validationResult.setVersion(validationResult.getVersion() + 1);
+        } else {
+            validationResult = new ValidationResult();
+            validationResult.setUuid(UUID.randomUUID().toString());
+            validationResult.setSubmissionId(submissionId);
+            validationResult.setEntityUuid(submittableUuid);
+
+            validationResult.setVersion(1);
+
+            Map<ValidationAuthor, Boolean> expectedResults = new HashMap<>();
+            for (ValidationAuthor validationAuthor : Arrays.asList(ValidationAuthor.Core, ValidationAuthor.Biosamples,
+                    ValidationAuthor.Ena, ValidationAuthor.Taxonomy)) {
+                expectedResults.put(validationAuthor, false);
+            }
+            validationResult.setExpectedResults(expectedResults);
+        }
+
+        repository.save(validationResult);
 
         return validationResult;
-    }
-
-    /**
-     * ValidationResult versioning starts on 1 with increments of 1.
-     * @param submissionId
-     * @param entityUuid
-     * @return String version
-     */
-    public int getVersion(String submissionId, String entityUuid) {
-        List<ValidationResult> validationResults = repository.findBySubmissionIdAndEntityUuid(submissionId, entityUuid);
-
-        if (validationResults.size() > 0) {
-            List<Integer> versions = validationResults.stream()
-                    .map(validationResult -> Integer.valueOf(validationResult.getVersion()))
-                    .collect(Collectors.toList());
-
-            int max = Collections.max(versions);
-            int version = max + 1;
-
-            deleteObsoleteValidationResults(validationResults);
-
-            return version;
-        }
-        return 1;
-    }
-
-    private ValidationResult generateValidationResult(String submissionId, BaseSubmittable submittable, int version) {
-        ValidationResult validationResult = new ValidationResult();
-        validationResult.setUuid(UUID.randomUUID().toString());
-        validationResult.setSubmissionId(submissionId);
-        validationResult.setEntityUuid(submittable.getId());
-
-        validationResult.setVersion(version);
-
-        Map<Archive, Boolean> expectedResults = new HashMap<>();
-        for (Archive archive : Arrays.asList(Archive.BioSamples, Archive.Ena, Archive.ArrayExpress)) {
-            expectedResults.put(archive, false);
-        }
-        validationResult.setExpectedResults(expectedResults);
-        return validationResult;
-    }
-
-    private void deleteObsoleteValidationResults(List<ValidationResult> validationResults) {
-        repository.delete(validationResults);
     }
 }
