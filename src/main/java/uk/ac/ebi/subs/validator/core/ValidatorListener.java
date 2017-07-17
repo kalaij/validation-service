0d2378782a2e9708d866cb8c91868b169dcffed0
Karoly Erdos <karoly@ebi.ac.uk>
2017-07-17 11:11:59 +0100

    Remove unneeded import

--- src/main/java/uk/ac/ebi/subs/validator/core/ValidatorListener.java
+++ src/main/java/uk/ac/ebi/subs/validator/core/ValidatorListener.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.subs.data.submittable.Assay;
 import uk.ac.ebi.subs.data.submittable.AssayData;
