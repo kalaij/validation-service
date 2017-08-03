3f35e64c9f0950b25a053fd98d935539056b9248
Rolando Fernandez <rolando@ebi.ac.uk>
2017-08-03 10:29:36 +0100

    added externalCiTest gradle task

--- src/test/java/uk/ac/ebi/subs/validator/aggregator/ValidationResultServiceTest.java
+++ src/test/java/uk/ac/ebi/subs/validator/aggregator/ValidationResultServiceTest.java
@@ -3,12 +3,14 @@ package uk.ac.ebi.subs.validator.aggregator;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.subs.validator.aggregator.config.MongoDBDependentTest;
 import uk.ac.ebi.subs.validator.data.SingleValidationResult;
 import uk.ac.ebi.subs.validator.data.SingleValidationResultsEnvelope;
 import uk.ac.ebi.subs.validator.data.ValidationAuthor;
@@ -24,6 +26,7 @@ import java.util.UUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @EnableMongoRepositories(basePackageClasses = ValidationResultRepository.class)
+@Category(MongoDBDependentTest.class)
 @EnableAutoConfiguration
 @SpringBootTest(classes = ValidationResultService.class)
 public class ValidationResultServiceTest {
