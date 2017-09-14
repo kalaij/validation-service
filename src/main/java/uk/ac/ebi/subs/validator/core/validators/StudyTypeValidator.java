20c7458eb7ffbc7f7fca59fd1cdce3a76e3c4630
David Richardson <davidr@ebi.ac.uk>
2017-09-14 14:17:03 +0100

    study must have a study type

---
+++ src/main/java/uk/ac/ebi/subs/validator/core/validators/StudyTypeValidator.java
@@ -0,0 +1,30 @@
+package uk.ac.ebi.subs.validator.core.validators;
+
+import org.springframework.stereotype.Service;
+import uk.ac.ebi.subs.data.component.StudyDataType;
+import uk.ac.ebi.subs.data.submittable.Study;
+import uk.ac.ebi.subs.validator.data.SingleValidationResult;
+import uk.ac.ebi.subs.validator.data.ValidationStatus;
+
+@Service
+public class StudyTypeValidator {
+
+    private static final String FAIL_MESSAGE = "Study type is required";
+
+    /**
+     * A study describes the type of data it includes with the study type field
+     *
+     * @param study
+     * @param singleValidationResult
+     */
+    public void validate(Study study, SingleValidationResult singleValidationResult) {
+        StudyDataType studyDataType = study.getStudyType();
+
+        if (studyDataType == null) {
+            singleValidationResult.setMessage(FAIL_MESSAGE);
+            singleValidationResult.setValidationStatus(ValidationStatus.Error);
+        }
+
+
+    }
+}
