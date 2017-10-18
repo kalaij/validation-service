f4b3bac8c8539a510da420b5175f4e15c6669391
Karoly Erdos <karoly@ebi.ac.uk>
2017-10-18 13:53:57 +0100

    Add a main application file for the Validation Service

--- src/main/java/uk/ac/ebi/subs/validator/ValidationServiceApplication.java
+++ src/main/java/uk/ac/ebi/subs/validator/ValidationServiceApplication.java
@@ -3,19 +3,17 @@ package uk.ac.ebi.subs.validator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.system.ApplicationPidFileWriter;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
- * Entry point of the Validator Aggregator application.
+ * Entry point of the Validation Service application.
  *
- * Created by karoly on 08/05/2017.
+ * Created by karoly on 18/10/2017.
  */
 @SpringBootApplication
-@ComponentScan("uk.ac.ebi.subs.validator")
-public class AggregatorApplication {
+public class ValidationServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(AggregatorApplication.class);
+        SpringApplication springApplication = new SpringApplication(ValidationServiceApplication.class);
         ApplicationPidFileWriter applicationPidFileWriter = new ApplicationPidFileWriter();
         springApplication.addListeners(applicationPidFileWriter);
         springApplication.run(args);
