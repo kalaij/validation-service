3420b8a40ebbf38b3da84a634ef338cbc6182d64
Neil Goodgame <neilg@ebi.ac.uk>
2018-01-30 14:40:36 +0000

    Expands project references that belong to the same submission

---
+++ src/main/java/uk/ac/ebi/subs/validator/coordinator/StudyValidationMessageEnvelopeExpander.java
@@ -0,0 +1,32 @@
+package uk.ac.ebi.subs.validator.coordinator;
+
+import uk.ac.ebi.subs.data.component.ProjectRef;
+import uk.ac.ebi.subs.data.submittable.Study;
+import uk.ac.ebi.subs.repository.model.Project;
+import uk.ac.ebi.subs.repository.repos.submittables.ProjectRepository;
+import uk.ac.ebi.subs.validator.data.StudyValidationMessageEnvelope;
+
+public class StudyValidationMessageEnvelopeExpander extends ValidationMessageEnvelopeExpander<StudyValidationMessageEnvelope> {
+    ProjectRepository projectRepository;
+
+    public StudyValidationMessageEnvelopeExpander(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    void expandEnvelope(StudyValidationMessageEnvelope validationMessageEnvelope, String submissionId) {
+        final ProjectRef projectRef = validationMessageEnvelope.getEntityToValidate().getProjectRef();
+
+        Project project;
+
+        if (projectRef != null && projectRef.getAccession() != null && !projectRef.getAccession().isEmpty()) {
+            project = projectRepository.findFirstByAccessionOrderByCreatedDateDesc(projectRef.getAccession());
+        } else {
+            project = projectRepository.findFirstByTeamNameAndAliasOrderByCreatedDateDesc(projectRef.getTeam(), projectRef.getAlias());
+        }
+
+        if (addToValidationEnvelope(project,submissionId)) {
+            validationMessageEnvelope.setProject(project);
+        }
+    }
+}
