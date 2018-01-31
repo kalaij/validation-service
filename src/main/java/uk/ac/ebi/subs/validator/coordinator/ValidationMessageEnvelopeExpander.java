4597131b12ad14399bb087709d7ccdc582793fbf
Neil Goodgame <neilg@ebi.ac.uk>
2018-01-31 16:06:07 +0000

    Abstract class for expanding the ValidationMessageEnvelope

--- src/main/java/uk/ac/ebi/subs/validator/coordinator/ValidationMessageEnvelopeExpander.java
+++ src/main/java/uk/ac/ebi/subs/validator/coordinator/ValidationMessageEnvelopeExpander.java
@@ -1,19 +1,14 @@
 package uk.ac.ebi.subs.validator.coordinator;
 
-import uk.ac.ebi.subs.data.submittable.BaseSubmittable;
-import uk.ac.ebi.subs.data.submittable.Submittable;
 import uk.ac.ebi.subs.repository.model.StoredSubmittable;
-import uk.ac.ebi.subs.validator.data.AssayDataValidationMessageEnvelope;
 import uk.ac.ebi.subs.validator.data.ValidationMessageEnvelope;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public abstract class ValidationMessageEnvelopeExpander<T extends ValidationMessageEnvelope> {
     abstract void expandEnvelope(T validationMessageEnvelope, String submissionId);
 
     boolean addToValidationEnvelope(StoredSubmittable storedSubmittable, String submissionId) {
-        return storedSubmittable.getSubmission().getId().equals(submissionId) ? true : false;
+        return storedSubmittable != null && storedSubmittable.getSubmission().getId().equals(submissionId) ? true : false;
     }
 
 }
