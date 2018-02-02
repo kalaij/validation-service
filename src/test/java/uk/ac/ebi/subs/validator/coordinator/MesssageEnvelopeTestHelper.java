60cf258283d49c3bc7149e1a3c8f684a64688e52
Neil Goodgame <neilg@ebi.ac.uk>
2018-02-02 16:23:16 +0000

    Moved createdAndSaveSamples into this helper class as a static method

--- src/test/java/uk/ac/ebi/subs/validator/coordinator/MesssageEnvelopeTestHelper.java
+++ src/test/java/uk/ac/ebi/subs/validator/coordinator/MesssageEnvelopeTestHelper.java
@@ -3,11 +3,15 @@ package uk.ac.ebi.subs.validator.coordinator;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.ac.ebi.subs.data.component.Team;
 import uk.ac.ebi.subs.data.status.SubmissionStatusEnum;
+import uk.ac.ebi.subs.repository.model.Sample;
 import uk.ac.ebi.subs.repository.model.Submission;
 import uk.ac.ebi.subs.repository.model.SubmissionStatus;
 import uk.ac.ebi.subs.repository.repos.SubmissionRepository;
 import uk.ac.ebi.subs.repository.repos.status.SubmissionStatusRepository;
+import uk.ac.ebi.subs.repository.repos.submittables.SampleRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MesssageEnvelopeTestHelper {
@@ -30,6 +34,21 @@ public class MesssageEnvelopeTestHelper {
         Team team = new Team();
         team.setName(UUID.randomUUID().toString());
         return team;
+    }
+
+    static List<Sample> createAndSaveSamples (SampleRepository sampleRepository, Submission submission, Team team, int sampleNumber) {
+        List<Sample> sampleList = new ArrayList<>(sampleNumber);
+        for (int i = 0; i < sampleNumber; i++ ) {
+            Sample sample = new Sample();
+            sample.setTeam(team);
+            String alias = UUID.randomUUID().toString();
+            String accession = UUID.randomUUID().toString();
+            sample.setAlias(alias);
+            sample.setAccession(accession);
+            sample.setSubmission(submission);
+            sampleList.add(sampleRepository.save(sample));
+        }
+        return sampleList;
     }
 
 
