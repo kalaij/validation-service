abb7ff0f971a7505af86af7700c96be9250568b2
Neil Goodgame <neilg@ebi.ac.uk>
2018-02-02 16:20:52 +0000

    removed call to addToValidationEnvelope added expansion of sample ref

--- src/main/java/uk/ac/ebi/subs/validator/coordinator/AssayDataValidationMessageEnvelopeExpander.java
+++ src/main/java/uk/ac/ebi/subs/validator/coordinator/AssayDataValidationMessageEnvelopeExpander.java
@@ -2,16 +2,23 @@ package uk.ac.ebi.subs.validator.coordinator;
 
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.subs.data.component.AssayRef;
+import uk.ac.ebi.subs.data.component.SampleRef;
+import uk.ac.ebi.subs.data.submittable.Assay;
+import uk.ac.ebi.subs.data.submittable.Sample;
 import uk.ac.ebi.subs.repository.repos.submittables.AssayRepository;
+import uk.ac.ebi.subs.repository.repos.submittables.SampleRepository;
 import uk.ac.ebi.subs.validator.data.AssayDataValidationMessageEnvelope;
+import uk.ac.ebi.subs.validator.model.Submittable;
 
 @Service
 public class AssayDataValidationMessageEnvelopeExpander extends ValidationMessageEnvelopeExpander<AssayDataValidationMessageEnvelope> {
 
     AssayRepository assayRepository;
+    SampleRepository sampleRepository;
 
-    public AssayDataValidationMessageEnvelopeExpander(AssayRepository assayRepository) {
+    public AssayDataValidationMessageEnvelopeExpander(AssayRepository assayRepository, SampleRepository sampleRepository) {
         this.assayRepository = assayRepository;
+        this.sampleRepository = sampleRepository;
     }
 
     @Override
@@ -26,9 +33,21 @@ public class AssayDataValidationMessageEnvelopeExpander extends ValidationMessag
             assayStoredSubmittable = assayRepository.findFirstByTeamNameAndAliasOrderByCreatedDateDesc(assayRef.getTeam(), assayRef.getAlias());
         }
 
-        if (addToValidationEnvelope(assayStoredSubmittable,submissionId)) {
-            assayDataValidationMessageEnvelope.setAssay(assayStoredSubmittable);
+        Submittable<Assay> assaySubmittable = new Submittable<>(assayStoredSubmittable,submissionId);
+        assayDataValidationMessageEnvelope.setAssay(assaySubmittable);
+
+        final SampleRef sampleRef = assayDataValidationMessageEnvelope.getEntityToValidate().getSampleRef();
+
+        uk.ac.ebi.subs.repository.model.Sample sampleStoredSubmittable;
+
+        if (sampleRef.getAccession() != null && !sampleRef.getAccession().isEmpty()) {
+            sampleStoredSubmittable = sampleRepository.findFirstByAccessionOrderByCreatedDateDesc(sampleRef.getAccession());
+        } else {
+            sampleStoredSubmittable = sampleRepository.findFirstByTeamNameAndAliasOrderByCreatedDateDesc(sampleRef.getTeam(), sampleRef.getAlias());
         }
+
+        Submittable<Sample> sampleSubmittable = new Submittable<>(sampleStoredSubmittable,submissionId);
+        assayDataValidationMessageEnvelope.setSample(sampleSubmittable);
 
     }
 
