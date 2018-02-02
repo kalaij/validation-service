cb71665f1ae7c4aed88fc02aa1d2e97183e22462
Neil Goodgame <neilg@ebi.ac.uk>
2018-02-02 16:53:23 +0000

    New test for the ReferenceValidator

---
+++ src/test/java/uk/ac/ebi/subs/validator/core/validators/ReferenceValidatorTest.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.subs.validator.core.validators;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReferenceValidatorTest {
+    @Test
+    public void validate() throws Exception {
+    }
+
+    @Test
+    public void validate1() throws Exception {
+    }
+
+}
