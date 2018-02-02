c3c744816911352bbc5c01ed59071d27f42eba33
Neil Goodgame <neilg@ebi.ac.uk>
2018-02-02 16:17:08 +0000

    Removed the method to check if the submission id matched the submittable submission id

--- src/main/java/uk/ac/ebi/subs/validator/coordinator/ValidationMessageEnvelopeExpander.java
+++ src/main/java/uk/ac/ebi/subs/validator/coordinator/ValidationMessageEnvelopeExpander.java
@@ -1,14 +1,9 @@
 package uk.ac.ebi.subs.validator.coordinator;
 
-import uk.ac.ebi.subs.repository.model.StoredSubmittable;
 import uk.ac.ebi.subs.validator.data.ValidationMessageEnvelope;
 
 
 public abstract class ValidationMessageEnvelopeExpander<T extends ValidationMessageEnvelope> {
     abstract void expandEnvelope(T validationMessageEnvelope, String submissionId);
-
-    boolean addToValidationEnvelope(StoredSubmittable storedSubmittable, String submissionId) {
-        return storedSubmittable != null && storedSubmittable.getSubmission().getId().equals(submissionId) ? true : false;
-    }
 
 }
