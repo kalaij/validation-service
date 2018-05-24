cc3a432e3b1f866b71c99c8df45e3bcf11277980
Flávia Penim <fpenim@ebi.ac.uk>
2018-05-24 14:41:59 +0100

    Bump version to 2.6.0

--- src/main/java/uk/ac/ebi/subs/validator/schema/README.md
+++ src/main/java/uk/ac/ebi/subs/validator/schema/README.md
@@ -12,3 +12,7 @@ The schema validator throws specific exceptions for unexpected errors:
 
 - `JsonSchemaValidatorException` for any error when reaching the *json-schema-validator* server.
 - `SchemaNotFoundException` when it can't find the schema to send for validation on the *json-schema-validator* server.
+
+## Schemas
+The schema validator does not hold the schemas used for the validation, these are available on the
+validation schemas [repository](https://github.com/EMBL-EBI-SUBS/validation-schemas).
--- src/main/java/uk/ac/ebi/subs/validator/schema/SchemaService.java
+++ src/main/java/uk/ac/ebi/subs/validator/schema/SchemaService.java
@@ -5,9 +5,6 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import uk.ac.ebi.subs.data.submittable.Assay;
-import uk.ac.ebi.subs.data.submittable.AssayData;
-import uk.ac.ebi.subs.data.submittable.Study;
 import uk.ac.ebi.subs.validator.schema.custom.CustomHttpMessageConverter;
 import uk.ac.ebi.subs.validator.schema.custom.SchemaNotFoundException;
 
